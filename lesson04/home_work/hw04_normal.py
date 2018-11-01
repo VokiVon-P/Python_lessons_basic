1f4c6361060dc30a4f7fcde41be48fbe0e3ece9e
Vokivon <vokivon.p@gmail.com>
2018-11-01 21:57:10 +0300

    Задача 3 из нормал готова

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -75,3 +75,55 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+
+import random
+import os
+
+print("---------- Задача 3 ----------")
+
+def write_file(file, num_text):
+    # path = os.path.join('', file)
+    with open(file, "w") as f:
+        f.write(num_text)
+
+def read_file(file):
+    # path = os.path.join('', file)
+    with open(file, "r") as f:
+        return f.read()
+
+
+def numgen():
+    txt = str(random.randint(1, 9))
+    for i in range(2500):
+        txt += str(random.randint(0, 9))
+    return txt
+
+"""
+Функцию ниже взял из разбора дз, тк мой вариант был похож, но не оформлен
+в отдельную функцию и выполнялся в теле модуля
+"""
+def max_repeated_digit(nums):
+    max_rep = 0
+    max_digit = nums[0]
+    digit = nums[0]
+    count_of_repeats = 0
+
+    for i in nums:
+        if i == digit:
+            count_of_repeats += 1
+            if count_of_repeats > max_rep:
+                max_rep = count_of_repeats
+                max_digit = digit
+        else:
+            digit = i
+            count_of_repeats = 0
+
+    return max_digit, max_rep + 1
+
+
+filename = "numtext.txt"
+res = numgen()
+write_file(filename, res)
+print(max_repeated_digit(read_file(filename)))
+
