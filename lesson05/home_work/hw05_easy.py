dfeecabee41b5604c2b4f5f655860c8696ce327e
Vokivon <vokivon.p@gmail.com>
2018-11-04 23:48:44 +0300

    Нормал готово

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -6,9 +6,10 @@ import shutil
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 def dir_in_cwd():
-    print("---------- Задача 2 ----------")
-    files = [i for i in os.listdir(path=os.getcwd()) if not '.' in i]
-    print(files)
+    # print("---------- Задача 2 ----------")
+    dirs = [ i for i in os.listdir(path=os.getcwd()) if os.path.isdir(i)]
+    print(dirs)
+    print()
 
 
 # Задача-1:
@@ -19,11 +20,11 @@ def dir_in_cwd():
 # Создаем новую директорию
 def make_dir(dir_name):
     dir_path = os.path.join(os.getcwd(), dir_name)
-    print(f"Создаем директорию {dir_path}")
     try:
         os.mkdir(dir_path)
+        print(f"Создана директория {dir_path}")
     except FileExistsError:
-        print('Такая директория уже существует')
+        print(f"Ошибка создания : Директория {dir_path} уже существует")
 
 
 # Удаление директории
@@ -32,23 +33,36 @@ def del_dir(dir_name):
     try:
         os.rmdir(dir_path)
         print(f"Удалена директория {dir_path}")
-    except FileExistsError:
+    except FileNotFoundError:
         print(f"Ошибка удаления : Директории {dir_path} не существует")
 
+# Перейти в директорию
+# для дз уровня нормал
+def ch_dir(dir_name):
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        os.chdir(dir_path)
+        print(f"Перешли в директорию {dir_path}")
+    except FileNotFoundError:
+        print(f"Ошибка перехода : Директории {dir_path} не существует")
 
-for i in range(10):
-    make_dir(f'NewDir_{i}')
-dir_in_cwd()
-input("Нажмите Enter для удаления созданных директорий")
-for i in range(10):
-    del_dir(f'NewDir_{i}')
-print()
-dir_in_cwd()
 
-# Задача-3:
-# Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
-print("---------- Задача 3 ----------")
-print()
-print(f"Корируем файл {__file__}")
-shutil.copyfile(__file__, f"{__file__}_copy")
-print(os.listdir(os.getcwd()))
+if __name__ == '__main__':
+    # выполняетя только если основной модуль выполнения
+    # при импорте не выполняется
+    for i in range(10):
+        make_dir(f'NewDir_{i}')
+    dir_in_cwd()
+    input("Нажмите Enter для удаления созданных директорий")
+    for i in range(10):
+        del_dir(f'NewDir_{i}')
+    print()
+    dir_in_cwd()
+
+    # Задача-3:
+    # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+    print("---------- Задача 3 ----------")
+    print()
+    print(f"Корируем файл {__file__}")
+    shutil.copyfile(__file__, f"{__file__}_copy")
+    print(os.listdir(os.getcwd()))
